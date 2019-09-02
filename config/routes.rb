a83fee5cfe65cb7508396aa7b0aa4a9c2325e32f
Chris Goodwin <chris_2190@hotmail.com>
2019-09-02 17:51:43 +1000

    Maps Controller created

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   resources :posts, only: [:new, :create]
   resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "map", to: "maps#map"
 end
