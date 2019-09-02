84060fe2ff3dcd63e0f638180f4942d5d929714d
Gael Jerop <gael.jerop@yahoo.com>
2019-09-02 15:40:44 +1000

    created posts controller and new,create views

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,21 @@
+class PostsController < ApplicationController
+  def new
+    @user = User.find(params[:user_id])
+    @post = Post.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:content, :lng, :lat, :photo)
+  end
+end
