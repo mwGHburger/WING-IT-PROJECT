6aa69c20d79c1df50753eb1a7d96196c3ea7442f
Michael Wang <michaelw0395@gmail.com>
2019-09-04 00:26:15 +1000

    Wrote js function to determine lat and lng of user location

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,5 +3,8 @@ import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 // internal imports
 import { initMapbox } from '../plugins/init_mapbox';
+import { getCurrentPosition } from '../components/get_current_location';
 
 initMapbox();
+getCurrentPosition();
+
