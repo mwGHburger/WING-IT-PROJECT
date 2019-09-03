dbd6a18e66393db45bf1b25bb1bd715a07e9a4fc
Hien Nguyen <hiennguyen1529@gmail.com>
2019-09-03 11:42:25 +1000

    added map to controller and map show

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,5 +1,12 @@
 class MapsController < ApplicationController
   def show
     @posts = Post.all
+
+    @markers = @posts.map do |post|
+      {
+        lat: post.latitude,
+        lng: post.longitude
+      }
+    end
   end
 end
